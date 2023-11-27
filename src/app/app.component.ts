2a747986fe38a8109feb71f084e96e78e6d2fc23
1lostsun <149478863+1lostsun@users.noreply.github.com>
2023-11-27 15:00:49 +0300

    site update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'app_prj';
+  search: string = '123123';
+
+  handleSearch() {
+    console.log(123)
+    console.log(this.search)
+  }
 }

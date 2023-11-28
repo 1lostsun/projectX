d0116a90e47954bbf4985f7990f48b261617355d
1lostsun <149478863+1lostsun@users.noreply.github.com>
2023-11-28 17:23:02 +0300

    all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,40 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { GoogleBooksAPI } from "google-books-js";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FormsModule],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Books Searcher';
-  search: string = '';
+  search = new FormControl('');
+  currentSearch: string | null = null;
+  items: any = [];
+  googleBooksApi = new GoogleBooksAPI();
 
-  handleSearch() {
-    console.log(123)
-    console.log(this.search)
+  async handleSearch() {
+    console.log(this.search.value);
+    if (this.currentSearch === this.search.value) return;
+
+    const result = await this.googleBooksApi.search({
+      filters: {
+        title: this.search.value || '',
+      },
+    });
+
+    const test = this.googleBooksApi.getVolume("uaoREAAAQBAJ");
+
+    console.log(test)
+
+    this.currentSearch = this.search.value;
+    this.items = result.items;
+  
+    console.log(result);
   }
 }
